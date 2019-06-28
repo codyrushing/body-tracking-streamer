9dd824a5438d51e1bb760d88e997d19dfe7edde0
Cody Rushing <codyrushing@gmail.com>
2019-06-27 23:16:44 -0500

    a very bad WIP

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
-const { spawn } = require('child_process');
-const path = require('path');
-const { watch } = require('fs');
-const fs = require('mz/fs');
-const rimraf = require('rimraf');
-const streamer = require('./streamer');
-const { OPENPOSE_PATH, OPENPOSE_BIN_PATH, DATA_DIR, NET_RESOLUTION } = require('./config');
+const VideoReader = require('./lib/video-reader');
 
-const dataDirectoryPath = path.join(__dirname, DATA_DIR);
+(async function(){
+  const videoReader = new VideoReader({
+    src: './demo-video/professor.mp4'
+  });
 
-const clean = () => {
-  return new Promise(
-    resolve => rimraf(path.join(dataDirectoryPath, '*'), resolve)
-  )
-};
+  await videoReader.decode();
 
-const go = async () => {
-  await clean();
-  const OP = spawn(
-    OPENPOSE_BIN_PATH,
-    [
-      '--disable_multi_thread',
-      '--net_resolution', NET_RESOLUTION,
-      '--output_resolution', '320x-1',
-      '--keypoint_scale', 3,
-      '--render_threshold', 0.05,
-      '--render_pose', 0,
-      '--camera', 1,
-      '--write_json', dataDirectoryPath,
-      '--display', 0
-    ],
-    {
-      cwd: OPENPOSE_PATH
-    }
-  );
+  if(videoReader.frameStream){
+    videoReader.frameStream
+      .on(
+        'data',
+        console.log
+      );
+  }
 
-  require('./websocket-server');
-
-  OP.stdout.on('data', data => console.log(data.toString()));
-  OP.stderr.on('data', data => console.error(data.toString()));
-  OP.on('error', console.error);
-
-  watch(
-    dataDirectoryPath,
-    (type, file) => {
-      if(type === 'change'){
-        streamer.write(path.join( dataDirectoryPath, file));
-      }
-    }
-  );
-};
-
-go();
+})();
